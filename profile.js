597542a87c46bf19c90bdefdda3603b7617d12cf
Hajar-lyoubi <80346439+Hajar-lyoubi@users.noreply.github.com>
2021-04-10 15:20:18 +0200

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,96 +1,33 @@
 import React, { Component } from 'react';
+import { AppRegistry,View,Text,StyleSheet } from 'react-native';
 
-import { AppRegistry, StyleSheet, ActivityIndicator,  Text, View, Alert } from 'react-native';
-import ListView from "deprecated-react-native-listview";
+export default class home extends Component{
+static navigationOptions= ({navigation}) =>({
+		  title: 'Welcome',
+	});
 
-export default class Myproject extends React.Component {
+	render(){
+		const { navigate } = this.props.navigation;
+		return(
+	  <View style={styles.container}>
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    }
-  }
+	   <Text style={styles.pageName}>profile</Text>
 
-
-  componentDidMount() {
-
-    return fetch('http://172.20.10.2:8080/webApp/profile.php')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.setState({
-          isLoading: false,
-          dataSource: ds.cloneWithRows(responseJson),
-        }, function() {
-          // In this block you can do something with new state.
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  ListViewItemSeparator = () => {
-    return (
-      <View
-        style={{
-          height: .5,
-          width: "100%",
-          backgroundColor: "#000",
-        }}
-      />
-    );
-  }
-
-
-  render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{flex: 1, paddingTop: 20}}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
-
-    return (
-
-      <View style={styles.MainContainer}>
-
-        <ListView
-
-          dataSource={this.state.dataSource}
-
-          renderSeparator= {this.ListViewItemSeparator}
-
-          renderRow={(rowData) =>
-            <Text style={styles.rowViewContainer}>{rowData.name}</Text>}
-
-        />
 
       </View>
-    );
-  }
+		);
+	}
 }
-
 const styles = StyleSheet.create({
+	container:{
+		display:'flex',alignItems:'center',
+		justifyContent:'center'
+	},
 
-MainContainer :{
+	pageName:{
+		margin:10,fontWeight:'bold',
+		color:'#000', textAlign:'center'
+	},
 
-// Setting up View inside content in Vertically center.
-justifyContent: 'center',
-flex:1,
-margin: 10
-
-},
-
-   rowViewContainer: {
-        fontSize: 20,
-        paddingRight: 10,
-        paddingTop: 10,
-        paddingBottom: 10,
-      }
 
 });
-
-AppRegistry.registerComponent('Myproject', () => Myproject);
--- register.js
+++ register.js
@@ -1,166 +1,125 @@
 import React, { Component } from 'react';
 import {
-AppRegistry,
-StyleSheet,
-Text,
-View,Button,TextInput,TouchableOpacity
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,Button,TextInput,TouchableOpacity
 } from 'react-native';
 
 
 export default class register extends Component {
-   static navigationOptions= ({navigation}) =>({
-    title: 'Register',
-    headerRight:
-    <TouchableOpacity
-    onPress={() => navigation.navigate('Home')}
-    style={{margin:10,backgroundColor:'orange',padding:10}}>
-    <Text style={{color:'#ffffff'}}>Home</Text>
-    </TouchableOpacity>
+	   static navigationOptions= ({navigation}) =>({
+		  title: 'Register',
+		  headerRight:
+		  <TouchableOpacity
+			onPress={() => navigation.navigate('Home')}
+			style={{margin:10,backgroundColor:'orange',padding:10}}>
+			<Text style={{color:'#ffffff'}}>Home</Text>
+		  </TouchableOpacity>
 
-});
+	});
 
-constructor(props){
-  super(props)
-  this.state={
-    userName:'',
-    userEmail:'',
-    userPassword:'',
-    userConfPassword:'',
-    userNumber:'',
-    userType:'',
-    userFiliere:''
+	constructor(props){
+		super(props)
+		this.state={
+			userName:'',
+			userEmail:'',
+			userPassword:''
+		}
+	}
+
+	userRegister = () =>{
+		
+		const {userName} = this.state;
+		const {userEmail} = this.state;
+		const {userPassword} = this.state;
+
+
+
+		fetch('http://172.20.10.2:80/webApp/registerr.php', {
+			method: 'post',
+			header:{
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body:JSON.stringify({
+				name: userName,
+				email: userEmail,
+				password: userPassword,
+			})
+
+		})
+		.then((response) => response.json())
+			.then((responseJson) =>{
+				alert(responseJson);
+			})
+			.catch((error)=>{
+				console.error(error);
+			});
+
+	}
+
+  render() {
+    return (
+	<View style={styles.container}>
+
+
+	  <TextInput
+	  placeholder="Enter Name"
+	  style={{width:250,margin:10, borderColor:"#333",
+	  borderWidth:1}}
+	  underlineColorAndroid="transparent"
+  onChangeText= {userName => this.setState({userName})}
+
+	  />
+
+	  <TextInput
+	  placeholder="Enter Email"
+	  style={{width:250,margin:10, borderColor:"#333",
+	  borderWidth:1}}
+	  underlineColorAndroid="transparent"
+	  onChangeText= {userEmail => this.setState({userEmail})}
+	  />
+
+	  <TextInput
+	  placeholder="Enter Password"
+	  style={{width:250,margin:10, borderColor:"#333",
+	  borderWidth:1}}
+	  underlineColorAndroid="transparent"
+	  onChangeText= {userPassword => this.setState({userPassword})}
+	  />
+
+	  <TouchableOpacity
+		onPress={this.userRegister}
+		style={{width:250,padding:10, backgroundColor:'magenta',
+		alignItems:'center'}}>
+	  <Text style={{color:'#fff'}}>Signup</Text>
+	  </TouchableOpacity>
+
+
+     </View>
+
+   );
   }
 }
 
-userRegister = () =>{
-  //alert('ok'); // version 0.48
-
-  const {userName} = this.state;
-  const {userEmail} = this.state;
-  const {userPassword} = this.state;
-  const {userConfPassword} = this.state;
-  const {userNumber} = this.state;
-  const {userType} = this.state;
-  const {userFiliere} = this.state;
-
-
-
-  fetch('http://172.20.10.2:8080/webApp/register.php',  {
-    method: 'post',
-    headers:{
-      'Accept': 'application/json',
-      'Content-type': 'application/json'
-    },
-    body:JSON.stringify({
-      name: userName,
-      email: userEmail,
-      password: userPassword,
-      confirm_password: userConfPassword,
-      number: userNumber,
-      user: userType,
-      filiere: userFiliere
-    })
-
-  })
-  .then((response) => response.json())
-    .then((responseJson) =>{
-      alert(responseJson);
-    })
-    .catch((error)=>{
-      console.error(error);
-    });
-
-}
-
-render() {
-  return (
-<View style={styles.container}>
-
-
-  <TextInput
-  placeholder="Enter Name"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-onChangeText= {userName => this.setState({userName})}
-
-  />
-
-  <TextInput
-  placeholder="Enter Email"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userEmail => this.setState({userEmail})}
-  />
-
-  <TextInput
-  placeholder="Enter Password"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userPassword => this.setState({userPassword})}
-  />
-  <TextInput
-  placeholder="Confirm Password"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userConfPassword => this.setState({userConfPassword})}
-  />
-  <TextInput
-  placeholder="Enter Number"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userNumber=> this.setState({userNumber})}
-  />
-  <TextInput
-  placeholder="Etes vous un parrain ou un filleul ?"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userType => this.setState({userType})}
-  />
-  <TextInput
-  placeholder="quel est votre filière ? ex : genie informatique"
-  style={{width:250,margin:10, borderColor:"#333",
-  borderWidth:1}}
-  underlineColorAndroid="transparent"
-  onChangeText= {userFiliere => this.setState({userFiliere})}
-  />
-
-  <TouchableOpacity
-  onPress={this.userRegister}
-  style={{width:250,padding:10, backgroundColor:'magenta',
-  alignItems:'center'}}>
-  <Text style={{color:'#fff'}}>Signup</Text>
-  </TouchableOpacity>
-
-
-   </View>
-
- );
-}
-}
-
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center',
-  backgroundColor: '#F5FCFF',
-},
-welcome: {
-  fontSize: 20,
-  textAlign: 'center',
-  margin: 10,
-},
-instructions: {
-  textAlign: 'center',
-  color: '#333333',
-  marginBottom: 5,
-},
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 AppRegistry.registerComponent('register', () => register);
